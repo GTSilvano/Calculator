2d53c081b0ff0287ff66c810fb373e4045827511
GTSilvano <silvano.condrea@gmail.com>
2022-08-08 15:32:28 +0300

    made it reusable until quiting

--- calculator.py
+++ calculator.py
@@ -1,31 +1,35 @@
 from secrets import choice
 
+while True:
 
-print("1 Add")
-print("2 Substract")
-print("3 Multiply")
-print("4 Divide")
+    print("1 Add")
+    print("2 Substract")
+    print("3 Multiply")
+    print("4 Divide")
+    print("Type Quit to Exit")
 
-choice = input("Choose your operation ")
+    choice = input("Choose your operation ")
+    if choice == "Quit":
+        break
 
-num1 = float(input("First Number "))
-num2 = float(input("Second Number "))
+    num1 = float(input("First Number "))
+    num2 = float(input("Second Number "))
 
-if choice == "1":
-    print(num1, "+", num2, "=", (num1+num2))
-elif choice == "2":
-    print(num1, "-", num2, "=", (num1-num2))
-elif choice == "3":
-    print(num1, "*", num2, "=", (num1*num2))
-elif choice == "4":
-    if num2 == 0.0:
-        print("You divided by zero you idiot sandwich!")
-    print(num1, "/", num2, "=" (num1/num2))
-else:
-    print("Add numbers Kemosabe!")
+    if choice == "1":
+        print(num1, "+", num2, "=", (num1+num2))
+    elif choice == "2":
+        print(num1, "-", num2, "=", (num1-num2))
+    elif choice == "3":
+        print(num1, "*", num2, "=", (num1*num2))
+    elif choice == "4":
+        if num2 == 0.0:
+            print("You divided by zero you idiot sandwich!")
+        print(num1, "/", num2, "=", (num1/num2))
+    else:
+        print("Select a proper operation Kemosabe!")
 
-def main():
-    
-    if __name__ == '__main__':
-        main()
+    def main():
+        
+        if __name__ == '__main__':
+            main()
 
